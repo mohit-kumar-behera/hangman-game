27b7ff796bfc130da1c35db9568068c18a040654
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-11-06 18:55:09 +0530

    turn camelcase to snakecase

--- src/hangman.py
+++ src/hangman.py
@@ -18,44 +18,44 @@ class hangman():
 		self.chosen_word = random.choice(word_list)
 		self.chosen_word_length = len(self.chosen_word)
 		self.lives = len(stages) - 1
-		self.gameIsFinished = False 
+		self.game_is_finished = False 
 
-	def makeAGuess(self):
+	def make_a_guess(self):
 		guess = input("Make a Letter Guess - ")
 		guess = guess.strip()
 		if len(guess) > 1:
-			return guess[len(guess)-1] # if user bymistakely types in more than one letter then it selects the last letter
+			return guess[len(guess) - 1] # if user bymistakely types in more than one letter then it selects the last letter
 		return guess # if user types only one letter
 
-	def fillTheLetter(self,guess,display):
+	def fill_the_letter(self, guess, display):
 		for position in range(self.chosen_word_length):
 			letter = self.chosen_word[position]
 			if letter == guess:
 				display[position] = letter
-		self.displayMessage(display)
+		self.display_message(display)
 
-	def displayMessage(self,display):
+	def display_message(self, display):
 		print(" ".join(display))
 
-	def isAllBlanksFilled(self,display):
+	def is_all_blanks_filled(self, display):
 		if "_" not in display:
-			self.gameIsFinished = True
+			self.game_is_finished = True
 			print("You Win the hangman game")
 
-	def drawHangmanArt(self,lives):
+	def draw_hangman(self, lives):
 		print(stages[lives])
 
-	def completedLetter(self,display):
-		numOfBlanks = display.count("_")
-		filledCharacter = self.chosen_word_length - numOfBlanks
-		self.displayMessage(display)
-		print(f"You have guessed {filledCharacter} words out of {self.chosen_word_length} words")
+	def completed_letter(self, display):
+		num_of_blanks = display.count("_")
+		filled_character = self.chosen_word_length - num_of_blanks
+		self.display_message(display)
+		print(f"You have guessed {filled_character} words out of {self.chosen_word_length} words")
 
-	def isLifeFinished(self,lives,display):
+	def is_life_finished(self, lives, display):
 		if lives == 0:
 			print("You Lose the hangman game")
-			self.gameIsFinished = True
-			self.completedLetter(display)
+			self.game_is_finished = True
+			self.completed_letter(display)
 
 	def play(self):
 		print(logo)
@@ -64,22 +64,22 @@ class hangman():
 		print("the word is ",self.chosen_word)
 		display = ['_'] * self.chosen_word_length
 		
-		while not self.gameIsFinished:
-			guess = self.makeAGuess()
+		while not self.game_is_finished:
+			guess = self.make_a_guess()
 			if guess in display: # guessed letter already exists
 				print(f"You already have guessed {guess}. Try some different letter")
 				continue
 		
 			# guessed letter is unique
 			if guess in self.chosen_word:
-				self.fillTheLetter(guess,display) # fill the guessed letter in the blanks and display 
-				#self.displayMessage(display)
-				self.isAllBlanksFilled(display)
+				self.fill_the_letter(guess, display) # fill the guessed letter in the blanks and display 
+				#self.display_message(display)
+				self.is_all_blanks_filled(display)
 			else:
 				self.lives -= 1
 				print(f"You guessed {guess}, that's not in the word. You lose a life.     Remaining Lives : {self.lives}")
-				self.drawHangmanArt(self.lives)
-				self.isLifeFinished(self.lives,display)
+				self.draw_hangman(self.lives)
+				self.is_life_finished(self.lives, display)
 
 hg = hangman()
 hg.play()
