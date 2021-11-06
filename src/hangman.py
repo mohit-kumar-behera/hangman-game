6bae48f3e3588b730be374c965d463ea5403f4a2
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-11-06 19:13:56 +0530

    colorise terminal

--- src/hangman.py
+++ src/hangman.py
@@ -1,4 +1,4 @@
-from hangman_art import logo,stages
+from hangman_art import logo, stages
 from hangman_words import word_list
 import random
 
@@ -10,8 +10,6 @@ class bcolors:
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 class hangman():
 	def __init__(self):
@@ -21,11 +19,12 @@ class hangman():
 		self.game_is_finished = False 
 
 	def make_a_guess(self):
-		guess = input("Make a Letter Guess - ")
+		guess = input(bcolors.WARNING + "Make a Letter Guess - " + bcolors.ENDC)
 		guess = guess.strip()
 		if len(guess) > 1:
-			return guess[len(guess) - 1] # if user bymistakely types in more than one letter then it selects the last letter
-		return guess # if user types only one letter
+			# if user bymistakely types in more than one letter then it selects the last letter
+			return guess[len(guess) - 1]
+		return guess
 
 	def fill_the_letter(self, guess, display):
 		for position in range(self.chosen_word_length):
@@ -35,49 +34,50 @@ class hangman():
 		self.display_message(display)
 
 	def display_message(self, display):
-		print(" ".join(display))
+		print(bcolors.OKBLUE + " ".join(display) + bcolors.ENDC)
 
 	def is_all_blanks_filled(self, display):
 		if "_" not in display:
 			self.game_is_finished = True
-			print("You Win the hangman game")
+			print(bcolors.OKGREEN + "You Win the hangman game" + bcolors.ENDC)
 
 	def draw_hangman(self, lives):
-		print(stages[lives])
+		print(bcolors.HEADER + stages[lives] + bcolors.ENDC)
 
 	def completed_letter(self, display):
 		num_of_blanks = display.count("_")
 		filled_character = self.chosen_word_length - num_of_blanks
 		self.display_message(display)
-		print(f"You have guessed {filled_character} words out of {self.chosen_word_length} words")
+		print(bcolors.OKCYAN + f"You have guessed {filled_character} words out of {self.chosen_word_length} words" + bcolors.ENDC)
 
 	def is_life_finished(self, lives, display):
 		if lives == 0:
-			print("You Lose the hangman game")
+			print(bcolors.FAIL + "You Lose the hangman game" + bcolors.ENDC)
 			self.game_is_finished = True
 			self.completed_letter(display)
 
 	def play(self):
 		print(logo)
 		print("-" * 50)
-		print(f"You have only {self.lives} lives to complete the game")
-		print("the word is ",self.chosen_word)
+		print(bcolors.OKCYAN + f"You have only {self.lives} lives to complete the game" + bcolors.ENDC)
 		display = ['_'] * self.chosen_word_length
 		
 		while not self.game_is_finished:
 			guess = self.make_a_guess()
-			if guess in display: # guessed letter already exists
-				print(f"You already have guessed {guess}. Try some different letter")
+			if guess in display: 
+				# guessed letter already exists
+				print(bcolors.OKCYAN + "You already have guessed " + bcolors.WARNING + guess + bcolors.ENDC + bcolors.OKCYAN +  ". Try some different letter" + bcolors.ENDC)
 				continue
 		
 			# guessed letter is unique
 			if guess in self.chosen_word:
-				self.fill_the_letter(guess, display) # fill the guessed letter in the blanks and display 
-				#self.display_message(display)
+				# correct guess
+				self.fill_the_letter(guess, display)
 				self.is_all_blanks_filled(display)
 			else:
+				# wrong guess
 				self.lives -= 1
-				print(f"You guessed {guess}, that's not in the word. You lose a life.     Remaining Lives : {self.lives}")
+				print(bcolors.FAIL + "You guessed " + bcolors.WARNING + guess + bcolors.ENDC + bcolors.FAIL + ", that's not in the word. You lose a life.\tRemaining Lives : " + bcolors.WARNING + str(self.lives) + bcolors.ENDC)
 				self.draw_hangman(self.lives)
 				self.is_life_finished(self.lives, display)
 
